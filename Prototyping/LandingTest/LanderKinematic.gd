467eb19ce3acd26be0fe9f15cb57664aff8cd462
Jack Partington <jap93@aber.ac.uk>
2023-02-16 12:01:35 +0000

    Parachute deployment mechanic developed

--- Prototyping/LandingTest/LanderKinematic.gd
+++ Prototyping/LandingTest/LanderKinematic.gd
@@ -1,5 +1,23 @@
-extends KinematicBody
+extends KinematicBody2D
 
+
+var velocity = Vector2.ZERO
+var gravity = 3.7
+var entry = Vector2(100,50)
+
+
+
+func _physics_process(delta):
+	if entry.y >= 0 && entry.x >= 0:
+		entry -= Vector2(100,50)
+	velocity.y += gravity * delta
+	velocity.y += entry.y
+	velocity.x += entry.x
+	
+	velocity = move_and_slide(velocity, Vector2(0, 0))
+	
+	
+	
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,7 +26,7 @@ extends KinematicBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
