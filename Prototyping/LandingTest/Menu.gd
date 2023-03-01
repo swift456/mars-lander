cba537c3cae9d25ff4bb246170ad4cac07c08b1c
Jack Partington <jap93@aber.ac.uk>
2023-03-01 12:06:57 +0000

    Very basic main menu implemented

--- Prototyping/LandingTest/Menu.gd
+++ Prototyping/LandingTest/Menu.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/Start.grab_focus()
 
 
 
@@ -18,4 +18,13 @@ func _ready():
 
 
 func _on_Start_pressed():
+	get_tree().change_scene("res://Main.tscn")
+	
+
+
+func _on_Settings_pressed():
 	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
